95c5a26510aaac46f9744843a162500a59545fdc
PedroPSG <46334439+pedrograngeiro@users.noreply.github.com>
2022-03-11 16:51:29 -0300

    fazendo novos exercicios

--- 02-Classes/2.2.2/PaintAppElipse.java
+++ 02-Classes/2.2.2/PaintAppElipse.java
@@ -1,75 +1,81 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Ellipse2D.Double;
 
-
-class PaintAppElipse {
-    public static void main(String[] args) {
-        PaintFrame2 frame = new PaintFrame2();
+class RectEllipseApp {
+    public static void main (String[] args) {
+        RectEllipseFrame frame = new RectEllipseFrame();
         frame.setVisible(true);
     }
 }
 
+class RectEllipseFrame extends JFrame {
+    Rect r1;
+    Ellipse e1;
 
-class PaintFrame2 extends JFrame {
-
-    Elipse e1;
-
-    PaintFrame2() {
-        this.addWindowListener (
+    RectEllipseFrame () {
+        this.addWindowListener(
             new WindowAdapter() {
                 public void windowClosing (WindowEvent e) {
                     System.exit(0);
                 }
             }
         );
-
-        this.setTitle("Painting Figures");
-        this.setSize(400, 400);
-        
-        this.e1 = new Elipse(50,350,100,250,150,270,200,200);
-
-        
+        this.setTitle("Rect + Ellipse");
+        this.setSize(350, 350);
+        this.r1 = new Rect(50,50, 100, 30);
+        this.e1 = new Ellipse(50,100, 100, 30);
     }
 
     public void paint(Graphics g) {
         super.paint(g);
-        this.e1.Paint(g);
+        this.r1.paint(g);
+        this.e1.paint(g);
+
     }
 }
 
-class Elipse {
-    int x1, y1, x2, y2, x3, y3, x4, y4;
+class Rect {
+    int x, y;
+    int w, h;
 
-    Elipse(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4) {
-        this.x1 = x1;
-        this.y1 = y1;
-        this.x2 = x2;
-        this.y2 = y2;
-        this.x3 = x3;
-        this.y3 = y3;
-        this.x4 = x4;
-        this.y4 = y4;
+    Rect (int x, int y, int w, int h) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
     }
 
-    void Print () {
-        System.out.format("Elipse na cordenada (x: %d, y: %d) (x: %d, y: %d) (x: %d, y: %d) (x: %d, y: %d)\n"
-        , this.x1, this.y1, this.x2, this.y2, this.x3, this.y3, this.x4, this.y4);
+    void print() {
+        System.out.format("Retangulo de tamanho (%d,%d) na posicao (%d,%d).\n",
+            this.w, this.h, this.x, this.y);
     }
 
-    void Paint(Graphics g) {
+    void paint (Graphics g) {
+        g.drawRect(this.x, this.y, this.w, this.h);
+    }
+}
+
+class Ellipse {
+    int x, y;
+    int w, h;
+
+    Ellipse (int x, int y, int w, int h) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+    }
+
+    void print() {
+        System.out.format("Elipse de tamanho (%d,%d) na posicao (%d,%d).\n",
+            this.w, this.h, this.x, this.y);
+    }
+
+    void paint(Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
-
-        g2d.setColor(Color.gray);
-        g2d.fillRect(0, 0, 400, 400);
-
-        g2d.setColor(Color.RED);
-        g2d.setStroke(new BasicStroke(3));
-        g2d.drawLine(50, 50, this.x1, this.y1);
-        g2d.drawLine(this.x1, this.y1, this.x2, this.y2);
-        g2d.drawLine(this.x2, this.y2, this.x3, this.y3);
-        g2d.drawLine(this.x3, this.y3, this.x4, this.y4);
-        
+        g2d.draw(new Ellipse2D.Double(this.x, this.y, this.w, this.h));
     }
-
 }
