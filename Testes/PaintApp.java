8b379c04f7dbf1545ab8165e1f1a48ceebd2485b
PedroPSG <46334439+pedrograngeiro@users.noreply.github.com>
2022-03-11 15:32:41 -0300

    Atualizando color background

--- Testes/PaintApp.java
+++ Testes/PaintApp.java
@@ -11,7 +11,7 @@ class PaintApp {
 }
 
 class PaintFrame extends JFrame {
-    Rect r1, r2, r3, r4;
+    Rect r1, r2, r3;
     
     PaintFrame () {
         this.addWindowListener (
@@ -23,23 +23,21 @@ class PaintFrame extends JFrame {
         );
         this.setTitle("Painting Figures");
         this.setSize(350, 350);
+        this.getContentPane().setBackground(new Color(237, 5, 118, 93));
         this.r1 = new Rect (50, 50, 100, 30, new Color(247, 5, 244), new Color(237,5, 118));
         this.r2 = new Rect (50, 100, 250, 60, new Color(116, 5, 224) , new Color(169, 5, 224));
         this.r3 = new Rect (150, 200, 150, 60, new Color(116, 5, 247), new Color(48, 5, 237));
-        this.r4 = new Rect(50, 250, 100, 30, Color.BLACK, Color.GREEN);
 
         // Mostrando a tela as posições
         this.r1.print();
         this.r2.print();
         this.r3.print();
-        this.r4.print();
     }
     public void paint (Graphics g) {
         super.paint(g);
         this.r1.paint(g);
         this.r2.paint(g);
         this.r3.paint(g);
-        this.r4.paint(g);
     }
 }
 
@@ -66,11 +64,13 @@ class Rect {
 
     void paint (Graphics g) {
         Graphics g2d = (Graphics2D) g;
+        
         g2d.setColor(corDaBorda);
         g2d.drawRect(this.x, this.y , this. w, this.h);
-
+        
         g2d.setColor(corDePreenchimento);
         g2d.fillRect(this.x, this.y, this.w, this.h);
+        
         
     }
 }
