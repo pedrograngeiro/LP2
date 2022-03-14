1f17a30a010237fe433e0359dac73bbded144081
PedroPSG <46334439+pedrograngeiro@users.noreply.github.com>
2022-03-14 14:54:39 -0300

    ex

--- 02-Classes/2.2.2/PaintAppElipse.java
+++ 02-Classes/2.2.2/PaintAppElipse.java
@@ -24,28 +24,36 @@ class RectEllipseFrame extends JFrame {
             }
         );
         this.setTitle("Rect + Ellipse");
+        this.getContentPane().setBackground(new Color(237, 5, 118, 93));
         this.setSize(350, 350);
-        this.r1 = new Rect(50,50, 100, 30);
-        this.e1 = new Ellipse(50,100, 100, 30);
+        this.r1 = new Rect(50,50, 100, 30, new Color(247, 5, 244), new Color(237,5, 118));
+        this.e1 = new Ellipse(50,100, 100, 30, new Color(116, 5, 247), new Color(48, 5, 237));
+
+        //Mostrando a posição na tela
+        this.r1.print();
+        this.e1.print();
     }
 
     public void paint(Graphics g) {
         super.paint(g);
         this.r1.paint(g);
         this.e1.paint(g);
-
     }
 }
 
 class Rect {
     int x, y;
     int w, h;
+    Color corDaBorda;
+    Color corDePreenchimento;
 
-    Rect (int x, int y, int w, int h) {
+    Rect (int x, int y, int w, int h, Color corDaborda, Color corDePreenchimento) {
         this.x = x;
         this.y = y;
         this.w = w;
         this.h = h;
+        this.corDaBorda = corDaborda;
+        this.corDePreenchimento = corDePreenchimento;
     }
 
     void print() {
@@ -54,19 +62,29 @@ class Rect {
     }
 
     void paint (Graphics g) {
-        g.drawRect(this.x, this.y, this.w, this.h);
+        Graphics g2d = (Graphics2D) g;
+
+        g2d.setColor(corDaBorda);
+        g2d.drawRect(this.x, this.y, this.w, this.h);
+
+        g2d.setColor(corDePreenchimento);
+        g2d.fillRect(this.x, this.y, this.w, this.h);
     }
 }
 
 class Ellipse {
     int x, y;
     int w, h;
+    Color corDaBorda;
+    Color corDePreenchimento;
 
-    Ellipse (int x, int y, int w, int h) {
+    Ellipse (int x, int y, int w, int h, Color corDaborda, Color corDePreenchimento) {
         this.x = x;
         this.y = y;
         this.w = w;
         this.h = h;
+        this.corDaBorda = corDaborda;
+        this.corDePreenchimento = corDePreenchimento;
     }
 
     void print() {
@@ -76,6 +94,11 @@ class Ellipse {
 
     void paint(Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
+        
+        g2d.setColor(corDaBorda);
         g2d.draw(new Ellipse2D.Double(this.x, this.y, this.w, this.h));
+
+        g2d.setColor(corDePreenchimento);
+        g2d.fillOval(this.x, this.y, this.w, this.h);
     }
 }
