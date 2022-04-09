3a1cd35b4d52efa610ee47fd21b56170083adcb2
Pedro Grangeiro <46334439+pedrograngeiro@users.noreply.github.com>
2022-04-09 20:42:57 -0300

    Exercicio 2.6.1

---
+++ 02-Classes/2.6.1/figures/Ellipse.java
@@ -0,0 +1,37 @@
+package figures;
+
+import java.awt.*;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Ellipse2D.Double;
+
+public class Ellipse extends Figure {
+    private int x, y;
+    private int w, h;
+    Color corDePreenchimento;
+    Color corDeBorda;
+
+    public Ellipse(int x, int y, int w, int h, Color corDePreenchimento, Color corDeBorda) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        this.corDePreenchimento = corDePreenchimento;
+        this.corDeBorda = corDeBorda;
+    }
+
+    public void print() {
+        System.out.format("Ellipse de tamanho (%d, %d) na posicao (%d, %d).\n",
+                this.w, this.h, this.x, this.y);
+    }
+
+    public void paint(Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+
+        g2d.setStroke(new BasicStroke(5));
+        g2d.setColor(corDeBorda);
+        g2d.draw(new Ellipse2D.Double(this.x, this.y, this.w, this.h));
+
+        g2d.setColor(corDePreenchimento);
+        g2d.fillOval(this.x, this.y, this.w, this.h);
+    }
+}
---
+++ 02-Classes/2.6.1/figures/Rect.java
@@ -0,0 +1,34 @@
+package figures;
+
+import java.awt.*;
+
+public class Rect extends Figure {
+    int x, y;
+    int w, h;
+    Color corDePreenchimento;
+    Color corDeBorda;
+
+    public Rect (int x, int y, int w, int h, Color corDePreenchimento, Color corDeBorda) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        this.corDePreenchimento = corDePreenchimento;
+        this.corDeBorda = corDeBorda;
+    }
+
+    public void print () {
+        System.out.format("Retangulo de tamanho (%d,%d) na posicao (%d,%d).\n",
+            this.w, this.h, this.x, this.y);
+    }
+
+    public void paint (Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+        
+        g2d.setStroke(new BasicStroke(5));
+        g2d.setColor(corDeBorda);
+        g2d.drawRect(this.x,this.y, this.w,this.h);
+        g2d.setColor(corDePreenchimento);
+        g2d.fillRect(this.x, this.y, this.w , this.h);
+    }
+}
